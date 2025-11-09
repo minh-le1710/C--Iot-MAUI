32ead17a53c224d88b4f8a5af12ceec0f2b4e84c
Minh_Le <minh1710@hcmut.edu.vn>
2025-11-09 12:27:13 +0700

    Update AioDashboard name

--- AioDashboard/Web.Client/obj/Debug/net9.0/Web.Client.AssemblyInfo.cs
+++ AioDashboard/Web.Client/obj/Debug/net9.0/Web.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04a7155a481d04d5230ab0e93a1de6740f3f23aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
